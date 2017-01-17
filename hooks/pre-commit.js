8f17051b50d8dfb47d1d0300a58e68da5f0fc0ef
liyongkai <liyongkai@xiaoyouzi.com>
2017-01-17 16:22:34 +0800

    添加检测冲突代码段

--- hooks/pre-commit.js
+++ hooks/pre-commit.js
@@ -11,6 +11,28 @@
 const execSync = require('child_process').execSync;
 const isConflictRegular = "^<<<<<<<\\s|^=======$|^>>>>>>>\\s"; // 冲突文件正则表达式
 
+
+
+
+// constructor(props) {
+//   super(props);
+//   this.state = {
+//     searchSwitch : false,
+//     loading : true,
+//     error : false,
+//     msg : ''
+//   };
+// <<<<<<< HEAD
+//   this.env = lib.env.get();                                                     // 获取环境变量，只能在在基类的构建函数调用完成之后获取，否则将取到错误的值，原生传过来的环境参数[test|release]
+// =======
+//   this.env = lib.env.set(props.env);  // 设置并获取环境变量，原生传过来的环境参数[test|release]
+// >>>>>>> 61eb31a7cda6e7aba343e6bbdf493a8a42f7858a
+//   this.domain = this.env.local ? 'http://local.youzibuy.tunnel.qydev.com' : ''; // 判断本地domain
+// }
+
+
+
+
 let results = null;
 
 // ref：https://github.com/zwhu/blog/issues/31
